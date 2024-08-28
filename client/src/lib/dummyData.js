689439f31790da9ccd5519382bb5e933562be2a2
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-28 13:25:47 +0200

    Create dummyData.js

---
+++ client/src/lib/dummyData.js
@@ -0,0 +1,76 @@
+export const  events = [
+    // Existing events
+    {
+        image: 'https://images.quicket.co.za/Q10_247_247.jpg',
+        title: 'TYGIELAND-BOSVERKIESING',
+        location: 'Laerskool Tygerpoort Skoolsaal',
+        date: 'Wednesday 28 August 2024',
+        time: '18:00',
+        id: "1123"
+    },
+    {
+        image: 'https://images.quicket.co.za/0627823_247_247.png',
+        title: 'MOO MOO MENLYN - PAINT & SIP',
+        location: 'Moo Moo Menlyn',
+        date: 'Thursday 29 August 2024',
+        time: '17:30',
+        id: "1124"
+    },
+    {
+        image: 'https://images.quicket.co.za/Q2_247_247.jpg',
+        title: 'INFUSION (2024 KWARTAAL)',
+        location: 'Communitas',
+        date: 'Thursday 18 July 2024',
+        time: '18:30',
+        id: "1125"
+    },
+    {
+        image: 'https://images.quicket.co.za/0620352_247_247.png',
+        title: 'PADSTAL TALENT (ROUND 1)',
+        location: 'Padstal',
+        date: 'Thursday 29 August 2024',
+        time: '20:00',
+        id: "1126"
+    },
+    // New events
+    {
+        image: 'https://images.quicket.co.za/0611459_247_247.png',
+        title: 'SUNSET JAZZ NIGHT',
+        location: 'The Jazz Lounge',
+        date: 'Friday 30 August 2024',
+        time: '19:00',
+        id: "1127"
+    },
+    {
+        image: 'https://images.quicket.co.za/0643963_247_247.png',
+        title: 'FOOD & WINE FESTIVAL',
+        location: 'Central Park',
+        date: 'Saturday 31 August 2024',
+        time: '11:00',
+        id: "1128"
+    },
+    {
+        image: 'https://images.quicket.co.za/0642081_247_247.png',
+        title: 'ARTISAN MARKET',
+        location: 'Market Square',
+        date: 'Sunday 1 September 2024',
+        time: '10:00',
+        id: "1129"
+    },
+    {
+        image: 'https://images.quicket.co.za/0641230_247_247.jpeg',
+        title: 'TECH INNOVATION CONFERENCE',
+        location: 'Convention Center',
+        date: 'Monday 2 September 2024',
+        time: '09:00',
+        id: "1130"
+    },
+    {
+        image: 'https://images.quicket.co.za/0641230_247_247.jpeg',
+        title: 'TECH INNOVATION CONFERENCE',
+        location: 'Convention Center',
+        date: 'Monday 2 September 2024',
+        time: '09:00',
+        id: "1130"
+    },
+  ];
