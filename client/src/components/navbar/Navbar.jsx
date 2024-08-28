b560022c17abe4864666a9983f411b9cee81d956
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-28 12:47:46 +0200

    Create Navbar

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./navbar.scss"
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className="wrapper">
+        <a href='/' className="logo">
+          <img src="https://www.quicket.co.za/content/images/static/01_PrimaryLogo_4c.svg" alt="" />
+        </a>
+
+        <ul>
+          <li>EVENTS</li>
+          <li>BROWSE EVENTS</li>
+          <li>LOGIN</li>
+        </ul>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
