6bad8391ae43b1c0c7477306a3c96b0ee571ccc3
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-29 13:20:11 +0200

    Update Login.jsx

--- client/src/routes/login/Login.jsx
+++ client/src/routes/login/Login.jsx
@@ -1,31 +1,95 @@
-import React from 'react'
-import "./login.scss"
-import { Box, Button, Checkbox, FormControlLabel, TextField } from '@mui/material'
-import { Link } from 'react-router-dom'
+import React, { useContext, useState } from "react";
+import "./login.scss";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  TextField,
+} from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import apiRequest from "../../lib/apiRequest";
 
 function Login() {
+  const [error, setError] = useState(false);
+  const [isLoadoing, setIsLoading] = useState(false);
+
+  const { updateUser } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (en) => {
+    en.preventDefault();
+    setError("");
+    setIsLoading(true);
+    const formData = new FormData(en.target);
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const res = await apiRequest.post("/auth/login", {
+        email,
+        password,
+      });
+      if (res.status === 200) {
+        updateUser(res.data);
+        navigate("/");
+      }
+      if (res.status === 400) {
+        console.log("USER INPUT ERROR");
+        console.log(res.status);
+        setError(res.error);
+      }
+    } catch (error) {
+      if (error.code == "ERR_NETWORK") {
+        console.log("ERR_NETWORK SERVER IS DOWN", ERR_NETWORK);
+      }
+      console.log(error);
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div className='login'>
-    <h3> Log in to Computicket</h3>
-  
-  <Box className="inputBox" sx={{ width: 500, maxWidth: '100%' }}>
-    <TextField fullWidth label="Email" id="email" />
-  </Box>
+    <div className="">
+      <form className="login" onSubmit={handleSubmit}>
+        <h3> Log in to Computicket</h3>
 
-  <Box className="inputBox" sx={{ width: 500, maxWidth: '100%' }}>
-    <TextField type="password" fullWidth label="Password" id="password" />
-  </Box>
+        <Box className="inputBox" sx={{ width: 500, maxWidth: "100%" }}>
+          <TextField name="email" fullWidth label="Email" id="email" />
+        </Box>
 
-  <Button sx={{ width: 500, maxWidth: '100%' }} variant="contained" size="medium">
-        Log in
-  </Button>
+        <Box className="inputBox" sx={{ width: 500, maxWidth: "100%" }}>
+          <TextField
+            name="password"
+            type="password"
+            fullWidth
+            label="Password"
+            id="password"
+          />
+        </Box>
 
-  <p><Link to={`/account/forgotpassword`}>Forgot your password?</Link> </p>
+        <Button
+          type="submit"
+          sx={{ width: 500, maxWidth: "100%" }}
+          variant="contained"
+          size="medium"
+        >
+          Log in
+        </Button>
 
-  <p>Don't have account? <Link to={`/register`}>Signup</Link> </p>
+        <p>
+          <Link to={`/account/forgotpassword`}>Forgot your password?</Link>{" "}
+        </p>
 
-  </div>
-  )
+        <p>
+          Don't have account? <Link to={`/register`}>Signup</Link>{" "}
+        </p>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
