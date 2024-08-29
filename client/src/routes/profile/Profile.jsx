24b77dc7c11f321c984f68c4621c1ecef0cb5273
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-29 13:20:52 +0200

    Create Profile.jsx

---
+++ client/src/routes/profile/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./profile.scss";
+
+function Profile() {
+  return (
+    <div className="profile">
+      <div className="wrapper"></div>
+    </div>
+  );
+}
+
+export default Profile;
