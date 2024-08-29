bd12c1eb46915ba5f93d25b6c63048b43b3a1383
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-29 13:20:41 +0200

    Update Navbar.jsx

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,24 +1,46 @@
-import React from 'react'
-import "./navbar.scss"
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 function Navbar() {
+  const { currentUser } = useContext(AuthContext);
+
+  // console.log(currentUser.user.firstName);
+
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="wrapper">
-        <a href='/' className="logo">
+        <a href="/" className="logo">
           <img src="/logo.png" alt="" />
         </a>
         <ul>
           <li>EVENTS</li>
-          <li><Link className='navLink' to={`/events`}>BROWSE EVENTS</Link></li>
-          <li><Link className='navLink' to={`/login`}>LOGIN</Link> </li>
+          <li>
+            <Link className="navLink" to={`/events`}>
+              BROWSE EVENTS
+            </Link>
+          </li>
+
+          {currentUser ? (
+            <>
+              <li>
+                <Link to={`/profile`}>
+                  {currentUser.user.firstName} <span>Profile</span>
+                </Link>
+              </li>
+            </>
+          ) : (
+            <li>
+              <Link className="navLink" to={`/login`}>
+                LOGIN
+              </Link>{" "}
+            </li>
+          )}
         </ul>
-
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
