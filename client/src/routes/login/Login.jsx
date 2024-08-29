4b0575eb23f84082298e516d8c006602f22a4a90
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-29 10:51:03 +0200

    Login and Register pages

--- client/src/routes/login/Login.jsx
+++ client/src/routes/login/Login.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import "./login.scss"
-
+import { Box, Button, Checkbox, FormControlLabel, TextField } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='login'>
+    <h3> Log in to Computicket</h3>
+  
+  <Box className="inputBox" sx={{ width: 500, maxWidth: '100%' }}>
+    <TextField fullWidth label="Email" id="email" />
+  </Box>
+
+  <Box className="inputBox" sx={{ width: 500, maxWidth: '100%' }}>
+    <TextField type="password" fullWidth label="Password" id="password" />
+  </Box>
+
+  <Button sx={{ width: 500, maxWidth: '100%' }} variant="contained" size="medium">
+        Log in
+  </Button>
+
+  <p><Link to={`/`}>Forgot your password?</Link> </p>
+
+  <p>Don't have account? <Link to={`/register`}>Signup</Link> </p>
+
+  </div>
   )
 }
 
--- client/src/routes/register/Register.jsx
+++ client/src/routes/register/Register.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import "./register.scss"
+import { Box, Button, Checkbox, FormControlLabel, TextField } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='register'>
+      <h3> Register on Computicket</h3>
+
+    <Box className="inputBox" sx={{ width: 500, maxWidth: '100%' }}>
+      <TextField fullWidth label="First Name" id="firstname" />
+    </Box>
+
+    <Box className="inputBox" sx={{ width: 500, maxWidth: '100%' }}>
+      <TextField fullWidth label="Last Name" id="lastname" />
+    </Box>
+
+    
+    <Box className="inputBox" sx={{ width: 500, maxWidth: '100%' }}>
+      <TextField fullWidth label="Email" id="email" />
+    </Box>
+
+    <Box className="inputBox" sx={{ width: 500, maxWidth: '100%' }}>
+      <TextField type="password" fullWidth label="Password" id="password" />
+    </Box>
+
+    <FormControlLabel className='checkBox' control={<Checkbox defaultChecked />} label="Daily sales updates (for organisers)" />
+
+    <Button sx={{ width: 500, maxWidth: '100%' }} variant="contained" size="medium">
+          Register
+    </Button>
+
+    <p>By clicking <b>Register</b>, I confirm that I agree with the <Link to={`/`}>Computicket terms of service</Link> </p>
+
+    <p>Already have a Quicket account? <Link to={`/login`}>  Log in</Link> </p>
+
+    </div>
   )
 }
 
