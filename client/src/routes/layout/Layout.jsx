c8d66fb114b0503ef9e1a3fc28415c53d2346a04
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-29 13:20:49 +0200

    Update Layout.jsx

--- client/src/routes/layout/Layout.jsx
+++ client/src/routes/layout/Layout.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar';
-import { Outlet } from 'react-router-dom';
+import React, { useContext } from "react";
+import Navbar from "../../components/navbar/Navbar";
+import { Navigate, Outlet } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 function Layout() {
-    return (
-        <div className="layout">
-          <div className="navbar">
-            <Navbar />
-          </div>
-          <div className="content">
-            <Outlet />
-          </div>
-        </div>
-      );
+  return (
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
-export default Layout
+function RequireAuth() {
+  const { currentUser } = useContext(AuthContext);
+
+  console.log(currentUser);
+
+  if (!currentUser) return <Navigate to="/login" />;
+  else {
+    return (
+      <div className="layout">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="content">
+          <Outlet />
+        </div>
+      </div>
+    );
+  }
+}
+
+export { Layout, RequireAuth };
