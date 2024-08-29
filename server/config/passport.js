dce30cc195b911f1d48f50969ef954e154d1d2ea
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-29 13:21:01 +0200

    Update passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,12 +1,11 @@
-const passport = require('passport');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const mongoose = require('mongoose');
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
 
-const keys = require('./keys');
+const keys = require("./keys");
 
-
-const User = mongoose.model('User');
+const User = mongoose.model("User");
 const secret = keys.jwt.secret;
 
 const opts = {};
@@ -16,22 +15,19 @@ opts.secretOrKey = secret;
 passport.use(
   new JwtStrategy(opts, (payload, done) => {
     User.findById(payload.id)
-      .then(user => {
+      .then((user) => {
         if (user) {
           return done(null, user);
         }
 
         return done(null, false);
       })
-      .catch(err => {
+      .catch((err) => {
         return done(err, false);
       });
   })
 );
 
-module.exports = async app => {
+module.exports = async (app) => {
   app.use(passport.initialize());
 };
-
-
-
