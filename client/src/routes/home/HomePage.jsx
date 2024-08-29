e8bdad47f2a68eda664276f99ce032c849dc9a25
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-29 13:20:44 +0200

    Update HomePage.jsx

--- client/src/routes/home/HomePage.jsx
+++ client/src/routes/home/HomePage.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import SearchBar from '../../components/searchBar/SearchBar'
-import EventsList from '../../components/eventsList/EventsList'
+import React, { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import EventsList from "../../components/eventsList/EventsList";
+import { AuthContext } from "../../context/AuthContext";
 
 function HomePage() {
+  const { currentUser } = useContext(AuthContext);
   return (
-    <div className='home'>
+    <div className="home">
       <div className="searchContainer">
         <SearchBar />
       </div>
 
       <div className="eventContainer">
-        <EventsList/>
+        <EventsList />
       </div>
-
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
