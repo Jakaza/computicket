760b2405201271cd52b0a9a597fa0381a2cefb5c
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-29 14:26:17 +0200

    Update Tabs.jsx
    change active class using state

--- client/src/components/ticket/Tabs.jsx
+++ client/src/components/ticket/Tabs.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./tabs.scss";
 
 function Tabs() {
+  const [activeTab, setActiveTab] = useState("UPCOMING");
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <div className="tabs">
-      <div className="tab active">UPCOMING</div>
-      <div className="tab">PROVISIONAL</div>
-      <div className="tab">PAST</div>
+      <div
+        className={`tab ${activeTab === "UPCOMING" ? "active" : ""}`}
+        onClick={() => handleTabClick("UPCOMING")}
+      >
+        UPCOMING
+      </div>
+      <div
+        className={`tab ${activeTab === "PROVISIONAL" ? "active" : ""}`}
+        onClick={() => handleTabClick("PROVISIONAL")}
+      >
+        PROVISIONAL
+      </div>
+      <div
+        className={`tab ${activeTab === "PAST" ? "active" : ""}`}
+        onClick={() => handleTabClick("PAST")}
+      >
+        PAST
+      </div>
     </div>
   );
 }
