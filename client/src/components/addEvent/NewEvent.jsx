c0637ccc4f7ef73cd8b36224d0abb2d4c55d7468
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-09-19 11:57:39 +0200

    new event inputs and and upload

--- client/src/components/addEvent/NewEvent.jsx
+++ client/src/components/addEvent/NewEvent.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import UploadWidget from '../uploadWidget/UploadWidget';
+import "./newEvent.scss";
 
 function NewEvent() {
+
+  const [value, setValue] = useState("");
+  const [images, setImages] = useState([]);
+
+  const handleSubmit = async (e) => {
+    const formData = new FormData(e.target);
+    const inputs = Object.fromEntries(formData);
+
+    log
+
+  }
+
+
+
   return (
     <div className="newPostPage">
       <div className="formContainer">
-        <h1>Add New Post</h1>
+        <h1>Add New Event</h1>
         <div className="wrapper">
-          <form >
+          <form onSubmit={handleSubmit}>
             <div className="item">
               <label htmlFor="title">Title</label>
               <input id="title" name="title" type="text" />
             </div>
             <div className="item">
-              <label htmlFor="price">Price</label>
+              <label htmlFor="price">General Price</label>
               <input id="price" name="price" type="number" />
             </div>
             <div className="item">
@@ -23,18 +41,12 @@ function NewEvent() {
               <label htmlFor="desc">Description</label>
               <ReactQuill theme="snow" onChange={setValue} value={value} />
             </div>
+
             <div className="item">
               <label htmlFor="city">City</label>
               <input id="city" name="city" type="text" />
             </div>
-            <div className="item">
-              <label htmlFor="bedroom">Bedroom Number</label>
-              <input min={1} id="bedroom" name="bedroom" type="number" />
-            </div>
-            <div className="item">
-              <label htmlFor="bathroom">Bathroom Number</label>
-              <input min={1} id="bathroom" name="bathroom" type="number" />
-            </div>
+
             <div className="item">
               <label htmlFor="latitude">Latitude</label>
               <input id="latitude" name="latitude" type="text" />
@@ -53,77 +65,59 @@ function NewEvent() {
               </select>
             </div>
             <div className="item">
-              <label htmlFor="type">Property</label>
+              <label htmlFor="type">Age Restriction</label>
               <select name="property">
-                <option value="apartment">Apartment</option>
-                <option value="house">House</option>
-                <option value="condo">Condo</option>
-                <option value="land">Land</option>
+                <option value="apartment">No</option>
+                <option value="house">Yes</option>
               </select>
             </div>
 
             <div className="item">
-              <label htmlFor="utilities">Utilities Policy</label>
-              <select name="utilities">
-                <option value="owner">Owner is responsible</option>
-                <option value="tenant">Tenant is responsible</option>
-                <option value="shared">Shared</option>
-              </select>
-            </div>
-            <div className="item">
-              <label htmlFor="pet">Pet Policy</label>
-              <select name="pet">
-                <option value="allowed">Allowed</option>
-                <option value="not-allowed">Not Allowed</option>
-              </select>
-            </div>
-            <div className="item">
-              <label htmlFor="income">Income Policy</label>
+              <label htmlFor="income">VIP Price</label>
               <input
-                id="income"
-                name="income"
+                id="vip"
+                name="vip"
                 type="text"
-                placeholder="Income Policy"
+                placeholder="Enter VIP price (0 if none)"
               />
             </div>
+
             <div className="item">
-              <label htmlFor="size">Total Size (sqft)</label>
-              <input min={0} id="size" name="size" type="number" />
+              <label htmlFor="income">VVIP Price</label>
+              <input
+                id="vvip"
+                name="vvip"
+                type="text"
+                placeholder="Enter VVIP price (0 if none)"
+              />
             </div>
+
+
             <div className="item">
-              <label htmlFor="school">School</label>
-              <input min={0} id="school" name="school" type="number" />
+              <label htmlFor="startDate">Start Date</label>
+              <input id="startDate" name="startDate" type="date" />
             </div>
-            <div className="item">
-              <label htmlFor="bus">bus</label>
-              <input min={0} id="bus" name="bus" type="number" />
-            </div>
-            <div className="item">
-              <label htmlFor="restaurant">Restaurant</label>
-              <input min={0} id="restaurant" name="restaurant" type="number" />
-            </div>
+
+           
             <button className="sendButton">Add</button>
-            {error && <span>error</span>}
+            {/* {error && <span>error</span>} */}
           </form>
         </div>
       </div>
       <div className="sideContainer">
 
-<h1>Side Menu</h1>
-
-
-        {/* {images.map((image, index) => (
+        {images.map((image, index) => (
           <img src={image} key={index} alt="" />
         ))}
         <UploadWidget
           uwConfig={{
             multiple: true,
-            cloudName: "lamadev",
-            uploadPreset: "estate",
-            folder: "posts",
+            cloudName: "dguxu4bqb",
+            uploadPreset: "computicket",
+            folder: "event",
           }}
           setState={setImages}
-        /> */}
+        />
       </div>
     </div>
   )
